8048af80718e594ccf849e3d65ab5a401c31b412
Thuy Luu <thuyluu@magenest.com>
2024-07-29 16:52:01 +0700

    delete

--- models/config_button.py
+++
@@ -1,5 +0,0 @@
-from odoo import models, fields
-
-
-class ConfigButton(models.Model):
-    _name = 'config.button.template'
--- models/message_template.py
+++
@@ -1,5 +0,0 @@
-from odoo import models
-
-
-class MessageTemplate(models.TransientModel):
-    _name = 'message.template'
